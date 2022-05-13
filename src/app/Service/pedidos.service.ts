6d23df9dd08ce29a40822bc22514ccdb964e58dd
HIBERUS\abarrasa <abarrasa@hiberus.com>
2022-05-13 13:32:42 +0200

    3

--- src/app/Service/pedidos.service.ts
+++ src/app/Service/pedidos.service.ts
@@ -10,4 +10,34 @@ export class PedidosService {
   constructor(private http: HttpClient) {
     this.pedidosUrl = 'http://localhost:8080/pedidos'
    }
+
+   public findAll(): Observable<Pedidos[]>{
+
+    return this.http.get<Pedidos[]>(`${this.pedidosUrl}/all`);
+
+  }
+
+  public findById(id: number): Observable<Pedidos>{
+
+     return this.http.get<Pedidos>(`${this.pedidosUrl}/getById/${id}`)
+
+  }
+
+  public updatePedidos(pedidos: Pedidos): Observable<Pedidos>{
+
+    return this.http.put<Pedidos>(`${this.pedidosUrl}/update`, pedidos)
+
+  }
+
+  public addPedidos(pedidos: Pedidos): Observable<Pedidos>{
+
+    return this.http.post<Pedidos>(`${this.pedidosUrl}/add`,pedidos)
+
+  }
+
+  public deletePedidos(id: number): Observable<Pedidos>{
+
+    return this.http.delete<Pedidos>(`${this.pedidosUrl}/delete/${id}`)
+
+  }
 }
--- src/app/Service/usuarios.service.ts
+++ src/app/Service/usuarios.service.ts
@@ -10,4 +10,35 @@ export class UsuariosService {
   constructor(private http: HttpClient) {
     this.usuariosUrl = 'http://localhost:8080/usuarios'
    }
+
+   
+   public findAll(): Observable<Usuarios[]>{
+
+    return this.http.get<Usuarios[]>(`${this.usuariosUrl}/all`);
+
+  }
+
+  public findById(id: number): Observable<Usuarios>{
+
+     return this.http.get<Usuarios>(`${this.usuariosUrl}/getById/${id}`)
+
+  }
+
+  public updateUsuarios(usuarios: Usuarios): Observable<Usuarios>{
+
+    return this.http.put<Usuarios>(`${this.usuariosUrl}/update`, usuarios)
+
+  }
+
+  public addUsuarios(usuarios: Usuarios): Observable<Usuarios>{
+
+    return this.http.post<Usuarios>(`${this.usuariosUrl}/add`,usuarios)
+
+  }
+
+  public deleteUsuarios(id: number): Observable<Productos>{
+
+    return this.http.delete<Productos>(`${this.productoUrl}/delete/${id}`)
+
+  }
 }
