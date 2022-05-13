d22d3148ec6ecebc026db53e441b48a10965fbe4
HIBERUS\abarrasa <abarrasa@hiberus.com>
2022-05-13 14:15:28 +0200

    d

--- src/app/Components/categorias/categorias.component.ts
+++ src/app/Components/categorias/categorias.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Categorias } from 'src/app/Classes/categorias';
+import { CategoriasService } from 'src/app/Service/categorias.service';
 
 @Component({
   selector: 'app-categorias',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categorias.component.css']
 })
 export class CategoriasComponent implements OnInit {
-
-  constructor() { }
+  categorias: Categorias[];
+  categoria: Categorias;
+  categoriaServices: CategoriasService;
+  constructor(categoriasService: CategoriasService) { }
 
   ngOnInit(): void {
+    this.getCategorias();
   }
 
-}
+    public getCategorias(): void {
+      this.categoriaServices.findAll().subscribe(
+        (response: Categorias[]) => {
+          this.categorias = response;
+          console.log(this.categorias);
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      );
+    }
+  }
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { Categorias } from './Classes/categorias';
+import { Router } from '@angular/router';
+import { CategoriasService } from './Service/categorias.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TiendaPC';
+  categoria: Categorias[];
+
+  constructor(private router: Router, private categoriaService: CategoriasService){}
+    getAutors(){
+      this.categoriaService.findAll().subscribe(data => {
+        this.categoria = data;
+      });
+    };
 }
