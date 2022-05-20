5c27e640941d5ebc8dd30d5443255c0d5b738f0b
adi <daw09.2022@gmail.com>
2022-05-20 12:23:51 +0200

    Funciones abrir y cerrar modal

--- src/app/Components/categorias/categorias.component.ts
+++ src/app/Components/categorias/categorias.component.ts
@@ -98,14 +98,31 @@ export class CategoriasComponent implements OnInit {
       container!.appendChild(button);
       button.click();
     }
-    public abrir(categoria: Categorias | null){
-      if(categoria){
+    public abrir(categoria: Categorias | null, mode: string){
+      if(mode == 'eliminar'){
         document.getElementById('id02').style.display = 'block';
         this.deletecategoria = categoria;
-      }else{
-        document.getElementById('id02').style.display = 'block';
+      }
+      if(mode == 'add'){
+        document.getElementById('id01').style.display = 'block';
+      }
+      if(mode == 'update'){
+        document.getElementById('id03').style.display = 'block';
+        this.updatecategoria = categoria;
       }
     }
+    public cerrar(mode: string){
+      if(mode == 'eliminar'){
+      document.getElementById('id02').style.display = 'none';
+    }
+    if(mode == 'add'){
+      document.getElementById('id01').style.display = 'none';
+    }
+    if(mode == 'update'){
+      document.getElementById('id03').style.display = 'none';
+    }
   }
+}
+
 
 
